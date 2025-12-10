5aef1d2eb901106a8302ea6dfd23ffb37807ec6b
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-12-10 18:05:35 +0200

    Ruff format check

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 import os
 
+
 class Settings(BaseModel):
     DATABASE_URL: str
     SECRET_KEY: str
@@ -9,8 +10,8 @@ class Settings(BaseModel):
     @classmethod
     def from_env(cls) -> "Settings":
         return cls(
-            DATABASE_URL=os.environ["DATABASE_URL"],
-            SECRET_KEY=os.environ["SECRET_KEY"]
+            DATABASE_URL=os.environ["DATABASE_URL"], SECRET_KEY=os.environ["SECRET_KEY"]
         )
+
 
 settings = Settings.from_env()
