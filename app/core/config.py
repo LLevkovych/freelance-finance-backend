2f98070ff1a1fb7c94d8f72989741de6e3ac9399
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-12-10 16:53:51 +0200

    Implement database configuration and migration setup with Alembic

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+class Settings(BaseModel):
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+
+settings = Settings()
